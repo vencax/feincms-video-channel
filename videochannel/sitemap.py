be1207f597e59d9305fada67aa8a8c7f5e056a3a
Vaclav Klecanda <vencax77@gmail.com>
2012-09-12 20:31:10 +0200

    sitemap

---
+++ videochannel/sitemap.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 12, 2012
+
+@author: vencax
+'''
+from django.contrib.sitemaps import Sitemap
+from feincms.module.medialibrary.models import MediaFile
+from feincms.content.application.models import app_reverse
+
+class VideoSitemap(Sitemap):
+    """Sitemap for the Video media file."""
+
+    def items(self):
+        return MediaFile.objects.filter(type__in=['video'])
+
+    def lastmod(self, obj):
+        return obj.created
+    
+    def location(self, item):
+        return app_reverse('videoch-detail', 'videochannel.urls', 
+                           kwargs={'mfile_id': item.id,})
