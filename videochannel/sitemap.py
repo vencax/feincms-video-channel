3ee9984ab334738bab589bdbc2ac87867f2d0fea
Vaclav Klecanda <vencax77@gmail.com>
2013-02-20 07:56:37 +0100

    sitemap fix

--- videochannel/sitemap.py
+++ videochannel/sitemap.py
@@ -4,19 +4,19 @@ Created on Sep 12, 2012
 @author: vencax
 '''
 from django.contrib.sitemaps import Sitemap
-from feincms.module.medialibrary.models import MediaFile
 from feincms.content.application.models import app_reverse
+from videochannel.models import Video
 
 
 class VideoSitemap(Sitemap):
     """Sitemap for the Video media file."""
 
     def items(self):
-        return MediaFile.objects.filter(type__in=['video'])
+        return Video.objects.all()
 
     def lastmod(self, obj):
-        return obj.created
+        return obj.mediaFile.created
 
     def location(self, item):
         return app_reverse('videoch-detail', 'videochannel.urls',
-                           kwargs={'mfile_id': item.id})
+                           kwargs={'slug': item.slug})
